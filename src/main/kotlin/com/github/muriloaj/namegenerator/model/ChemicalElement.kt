29fcb6980533e266010f706da579482ef1b2480e
MJ <muriloaj@gmail.com>
2018-05-13 23:05:36 -0300

    Create Chemical Element model based on CSV

---
+++ src/main/kotlin/com/github/muriloaj/namegenerator/model/ChemicalElement.kt
@@ -0,0 +1,8 @@
+package com.github.muriloaj.namegenerator.model
+
+data class ChemicalElement(val z: Long,
+                           val symbol: String,
+                           val latin: String,
+                           val portuguese: String,
+                           val spanish: String,
+                           val english: String)
