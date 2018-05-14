799e9e6d56ab686088285d72233207d9b2f3a529
MJ <muriloaj@gmail.com>
2018-05-13 22:33:17 -0300

    fix package name according content - controller

--- src/main/kotlin/com/github/muriloaj/namegenerator/controller/GreetingController.kt
+++ src/main/kotlin/com/github/muriloaj/namegenerator/controller/GreetingController.kt
@@ -1,4 +1,4 @@
-package com.github.muriloaj.namegenerator.service
+package com.github.muriloaj.namegenerator.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
