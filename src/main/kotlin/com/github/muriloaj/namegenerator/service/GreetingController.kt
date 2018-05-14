a45520c85b34c4b0070d843bb27184997fa9e1f7
MJ <muriloaj@gmail.com>
2018-05-13 21:09:17 -0300

    Add greeting service - hello world

---
+++ src/main/kotlin/com/github/muriloaj/namegenerator/service/GreetingController.kt
@@ -0,0 +1,14 @@
+package com.github.muriloaj.namegenerator.service
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+private class GreetingController {
+
+
+    @GetMapping("/greeting")
+    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) = "Hello $name"
+
+}
